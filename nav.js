48741b24c9d7eac39b90f87fb4bd6ba1ce490e64
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2023-08-03 15:16:13 -0700

    Nav
    Made the nav bar functional except for bringing you to different pages.

---
+++ nav.js
@@ -0,0 +1,16 @@
+$(function(){
+    // when burger is clicked, open nav
+    $("#burger").click(function(){
+        $(".nav").animate({width: "30%"}, 300).delay(50).promise().done(function(){
+            $(".x").css("display", "block");
+            $(".navLink").css("display", "flex");
+        });
+    });
+
+    // when x is clicked, close nav
+    $(".x").click(function(){
+        $(".x").css("display", "none");
+        $(".navLink").css("display", "none");
+        $(".nav").animate({width: "0%"}, 300);
+    });
+})
