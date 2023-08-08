8d30481b331e7e3d5e3fc5f0bd63a43a24da44f9
Noah-Santos <nsanto591@west-mec.org>
2023-08-07 17:38:49 -0700

    Stupid
    The images are being stupid and won't load

--- js/kyoto.js
+++ js/kyoto.js
@@ -3,13 +3,13 @@ $(function(){
     setInterval(function(){
         // gets the id of the image that is currently active and makes it hidden
         let currentActive = $(".active").attr("id");
-        // let currentActive2 = $(".active2").attr("id");
+        let currentActive2 = $(".active2").attr("id");
         $(`#${currentActive}`).removeClass("active").addClass("deactive");
-        // $(`#${currentActive2}`).removeClass("active2").addClass("deactive");
+        $(`#${currentActive2}`).removeClass("active2").addClass("deactive");
 
         // breaks the element id into an array
         let currentActiveArray = currentActive.split("");
-        // let currentActiveArray2 = currentActive2.split("");
+        let currentActiveArray2 = currentActive2.split("");
 
         // changes the images accordingly
         if(currentActiveArray[currentActiveArray.length-1] == 4){
@@ -20,18 +20,18 @@ $(function(){
             currentActiveArray[currentActiveArray.length-1] = num;
         }
         
-        // if(currentActiveArray2[currentActiveArray2.length-1] == 2){
-        //     currentActiveArray2[currentActiveArray2.length-1] = 1;
-        // }else{
-        //     let num = currentActiveArray2[currentActiveArray2.length-1];
-        //     num++
-        //     currentActiveArray2[currentActiveArray2.length-1] = num;
-        // }
+        if(currentActiveArray2[currentActiveArray2.length-1] == 2){
+            currentActiveArray2[currentActiveArray2.length-1] = 1;
+        }else{
+            let num = currentActiveArray2[currentActiveArray2.length-1];
+            num++
+            currentActiveArray2[currentActiveArray2.length-1] = num;
+        }
 
         // sets new image to active
         let newActive = currentActiveArray.join("");
-        // let newActive2 = currentActiveArray2.join("");
+        let newActive2 = currentActiveArray2.join("");
         $(`#${newActive}`).removeClass("deactive").addClass("active");
-        // $(`#${newActive2}`).removeClass("deactive").addClass("active2");
+        $(`#${newActive2}`).removeClass("deactive").addClass("active2");
     }, 3500);
 })
